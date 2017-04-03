9cee0f7f9aa78fc5207a11a992711961cf6b56ca
taljacobson <tal1989@gmail.com>
2017-04-04 00:06:50 +0300

    add typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+
+// Type definitions for react-native-share-menu  v1.4.0
+// Project: https://github.com/meedan/react-native-share-menu
+// Definitions by: Tal <https://github.com/taljacobson>
+// TypeScript Version: 2.1
+
+declare module "react-native-share-menu" { 
+    export default class ShareMenu {
+         static sharText(CB: (text:string) => any): void
+    } 
+}
